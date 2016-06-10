0119f5dd31ef0caa083610300ae89d1f4eda6976
Kamil Krawczyk <kamil.kraw@wp.pl>
2016-06-10 20:27:45 +0200

    Create Poczatek.R
    Mały początek, do wydobycia podstawowej informacji czyli imion i nazwisk uczestników dla komisji i powodu jej zwołania

---
+++ Poczatek.R
@@ -0,0 +1,71 @@
+# Początki
+
+# 1. Wczytanie potrzebnych pakietów
+# 2. Wczytanie stenogramu
+# 3. Podzielenie na akapity
+# 4. Wyodrębnienie konkretnych akapitów
+
+## 4.1. Akapity, które zawierają uczestników posiedzenia
+### a) Podzielenie akapitów na słowa
+### b) Wyodrębnienie z nich imion i nazwisk
+
+## 4.2. Akapit, który zawiera powód zwołania komisji
+### a) Wydobycie powodu zwołania obrad
+
+# .
+# .
+# .
+
+# Przedostatni punkt. Stworzenie ramki danych
+# Ostatni punkt. Przetworzenie ramki danych na plik csv 
+
+# Przykład:
+
+# 1.
+# Wczytajmy pakiety, które pomogą nam zdoyć środek html, jak również inne, które pomogą w późniejszych analizach
+
+library(RCurl)
+library(XML)
+library(ggplot2)
+
+# 2.
+# Potrzebny kod do wczytywania stenogramów! - skorzystanie z RCurl i XML
+
+stenogram <- "W posiedzeniu udział wzięli: Konstanty Radziwiłł minister zdrowia i Jarosław Pinkas sekretarz stanu w Ministerstwie Zdrowia ze współpracownikami, Andrzej Jacyna p.o. prezesa Narodowego Funduszu Zdrowia ze współpracownikiem, Krystyna Kozłowska rzecznik praw pacjenta ze współpracownikiem, Bartosz Sowiera dyrektor gabinetu Rzecznika w Biurze Rzecznika Praw Dziecka, Maciej Szustowicz wicedyrektor Departamentu Zdrowia Najwyższej Izby Kontroli, Dorota Budarz członek Rady Krajowej, Marcelina Zawisza członek Zarządu Krajowego i Marta Nowak rzecznik prasowy Partii Razem, Zdzisław Bujas wiceprzewodniczący Zarządu Krajowego Ogólnopolskiego Związku Zawodowego Pielęgniarek i Położnych, Wanda Fidelus-Ninkiewicz dyrektor Biura Naczelnej Izby Lekarskiej ze współpracownikiem, Jan Kowalczuk członek Zarządu Ogólnopolskiego Związku Zawodowego Lekarzy wraz ze współpracownikami, Zofia Małas prezes Naczelnej Rady Pielęgniarek i Położnych wraz ze współpracownikami Elżbieta Piotrowska-Rutkowska prezes Naczelnej Rady Aptekarskiej oraz Mateusz Moksik asystent przewodniczącego Komisji
+W posiedzeniu udział wzięli pracownicy Kancelarii Sejmu: Longina Grzegrzułka, Małgorzata Siedlecka-Nowak, Monika Żołnierowicz-Kasprzyk – z sekretariatu Komisji w Biurze Komisji Sejmowych."
+
+# 3.
+# Nowe linie(entery) R traktuje jako napis "\n", więc pozbędźmy się ich - strsplit z sapply
+# Od razu da nam to podzielenie tekstu na akapity
+
+akapity <- sapply(strsplit(stenogram,"\n"), as.character)
+
+# 4.
+# Każdy stenogram posiada akapit/y "W posiedzeniu udział [...]" , wybierzmy więc je - subset z grepl
+# - || - akapit i jemu następny np. " Komisja Zdrowia [...], rozpatrzyła: \n – informację na temat aktualnej sytuacji w Szpitalu Instytucie „Pomnik – Centrum Zdrowia Dziecka” "
+
+## 4.1.
+
+udzial <- subset(akapity,grepl("W posiedzeniu udział",akapity))
+
+### a)
+# Podzielmy dane akapity na słowa - sapply z strsplit 
+
+slowa <- sapply(strsplit(udzial," "), as.character)
+
+### b)
+# Stwórzmy wektor imion i przyłóżmy go, żeby wydobyć imiona.
+# Jako że po imionach występuje nazwisko dodajmy również kolejny wyraz - Pętla przeszukująca słowa w akapitach
+
+
+pomocniczy <- ""
+imiona <- c("Maciej","Mateusz","Jan","Wanda")
+for(i in 1:length(slowa)){
+  for(j in 1:length(slowa[[i]])){
+    if(slowa[[i]][j] %in% imiona){
+      pomocniczy <- paste(pomocniczy," ",slowa[[i]][j],slowa[[i]][j+1])
+    }
+  }
+}
+
+obecni <- sapply(strsplit(pomocniczy,"   "), as.character)[-1]
